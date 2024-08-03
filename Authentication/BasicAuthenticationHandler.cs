9665376bb8f0ccafb1081934f67c826ff1cb510b
Ossama Samir <147268769+EngOssamaSameer@users.noreply.github.com>
2024-08-03 22:44:02 +0300

    add

---
+++ Authentication/BasicAuthenticationHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace BasicAuthentication.Authentication
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return Task.FromResult(AuthenticateResult.NoResult());
+
+            var authHeader = Request.Headers["Authorization"].ToString();
+
+            if(!authHeader.StartsWith("Basic",StringComparison.OrdinalIgnoreCase))
+                return Task.FromResult(AuthenticateResult.Fail("Unkown Scheme"));
+
+            var encodeCred = authHeader["Basic".Length..];
+            var decodeCred = Encoding.UTF8.GetString(Convert.FromBase64String(encodeCred));
+
+            var cred = decodeCred.Split(":");
+
+            var role = "Admin";
+            if (cred[0] == "Nada")
+            {
+                role = "User";
+            }
+
+            // crate idenetity with claims
+            var identity = new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Role,role),
+                new Claim(ClaimTypes.Name,cred[0])
+            }, "Basic");
+
+            // principal
+            var principal = new ClaimsPrincipal(identity);
+
+            // Create Teckit
+            var teckit = new AuthenticationTicket(principal,"Basic");
+
+
+            return Task.FromResult(AuthenticateResult.Success(teckit));
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet("6")]
+        [Authorize(Roles ="Admin")]
+        public IActionResult GetAdmin()
+        {
+            var username = HttpContext.User.Identity.Name;
+
+            return Ok($"Done here is Admin DashBoard:{username}");
+        }
+        [HttpGet("7")]
+        [Authorize(Roles ="User")]
+        public IActionResult GetUser()
+        {
+            var username = HttpContext.User.Identity.Name;
+
+            return Ok($"Done here is User Page:{username}");
+        }
+
+    }
+}
